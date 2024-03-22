0051396d997fa5f5a34bff3a975514a130bb7012
eyob sisay <eyobsisay37@gmail.com>
2024-03-22 14:14:30 +0300

    MERN stack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBooks from './pages/CreateBooks';
@@ -11,8 +10,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
-      <Route path='/books/details/:id' element={<ShowBook/>} />
-      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
   );
